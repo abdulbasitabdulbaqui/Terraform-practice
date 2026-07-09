30e58597f1c51427d2a35b63973d19e3dcf02a6d
Ansari Abdul Basit <abdulbasitabdulbaqui@gmail.com>
2026-07-09 15:41:45 +0530

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "EC2 instance AMI ID"
+  type        = string
+  default     = "ami-03f1d2b3639314198"
+}
